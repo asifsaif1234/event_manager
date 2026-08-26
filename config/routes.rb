06c53886a33babb67b52d0c6e275baf2d73def5a
asif saif <asifsaif786@gmail.com>
2026-08-27 00:29:59 +0530

    Removing comment code from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
 
   get "/sign_in", to: redirect("https://guiding-grouper-6633.accounts.dev/sign-in")
   get "/sign_up", to: redirect("https://guiding-grouper-6633.accounts.dev/sign-up")
-  # delete '/sign_out', to: 'sessions#destroy'
   match "/sign_out", to: "sessions#destroy", via: [ :get, :delete ]
-  get "/auth/callback", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
